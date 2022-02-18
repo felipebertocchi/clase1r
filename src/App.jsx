dad77bd8008ed8adb39adbffcf30d570b3b2f647
felipebertocchi <fabertocchi@gmail.com>
2022-02-18 20:51:48 -0300

    pasar App a un Class Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
+import React, { Component } from 'react'
 import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
 import Clock from './components/Clock/Clock';
 import Saludo from './components/Saludo/Saludo'
 import Contador from './components/Contador/Contador';
 
-function App() {
-  const menuItems = ['Home', 'Marketplace', 'Cart', 'Login']
+export default class App extends Component {
 
-  const handleSubmitNumber = (contador) => {
+  menuItems = ['Home', 'Marketplace', 'Cart', 'Login']
+
+  handleSubmitNumber = (contador) => {
     console.log(contador)
   }
 
-  return (
-    <>
-      <Navbar items={menuItems} />
-      <Clock />
-      <Contador valorInicial={50} handleSubmitNumber={(contador) => handleSubmitNumber(contador)}/>
-      <Saludo nombre={'Pepe'} edad={32} apellido={'Gomez'} />
-      <Footer />
-    </>
-  )
+  render() {
+    return (
+      <>
+        <Navbar items={this.menuItems} />
+        <Clock />
+        <Contador valorInicial={50} handleSubmitNumber={(contador) => this.handleSubmitNumber(contador)} />
+        <Saludo nombre={'Pepe'} edad={32} apellido={'Gomez'} />
+        <Footer />
+      </>
+    )
+  }
 }
-
-export default App;
